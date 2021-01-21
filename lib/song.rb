84cddf3e04bce0b5cc9390be7cd76551ccf1d226
figo5530 <figooooo0221@gmail.com>
2021-01-21 14:59:12 -0500

    commit tree 6d50a443903002d35d0a8921f230438276d96065

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,61 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    self.new.save.last
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new
+    song.name = song_name
+    song  
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find {|s| s.name == song_name}
+  end
+
+  def self.find_or_create_by_name(name)
+    # if self.find_by_name(name)
+    #   self.find_by_name(name)
+    # else
+    #   self.create_by_name(name)
+    # end
+    self.find_by_name(name) or self.create_by_name(name)
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|s|s.name}
+  end
+
+  def self.new_from_filename(name)
+    file = name.split(" - ")
+    artist_name = file[0]
+    song_name = file[1].gsub(".mp3","")
+
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.create_from_filename(name)
+    file = name.split(" - ")
+    artist_name = file[0]
+    song_name = file[1].gsub(".mp3","")
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
